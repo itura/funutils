be4fc00f7bca1d59ea3de741c19eae4ff54ed689
agarrard <austin.w.garrard@gmail.com>
2020-04-21 08:41:50 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+const LazySequence = (startSequence) => {
+  const fns = []
+
+  const map = function (fn) {
+    fns.push(fn)
+    return this
+  }
+
+  const nil = Symbol('nil')
+  const filter = function (fn) {
+    fns.push(x => fn(x) ? x : nil)
+    return this
+  }
+
+  const compact = function () {
+    filter(x => {
+      const falsey = !x
+      const emptyArray = falsey || x.length === 0
+      const zero = x === 0
+
+      return !(falsey || emptyArray) || zero
+    })
+    return this
+  }
+
+  const take = n => {
+    const iterator = startSequence()
+
+    const results = []
+    for (let i = 0; i < n; i++) {
+      const next = iterator.next()
+      if (next.done) {
+        break
+      }
+
+      const result = fns.reduce(
+        (prev, fn) => prev === nil ? nil : fn(prev),
+        next.value
+      )
+
+      if (result !== nil) {
+        results.push(result)
+      }
+    }
+
+    return results
+  }
+  return {
+    map, filter, compact, take
+  }
+}
+
+module.exports = { LazySequence }
